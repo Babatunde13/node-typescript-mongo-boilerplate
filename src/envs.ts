d625108dd266c24d772df5f05f6ec2e69c29b216
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2023-01-17 18:08:39 +0100

    (feat): ensure env vars are vlalid (#2)

--- src/envs.ts
+++ src/envs.ts
@@ -10,7 +10,7 @@ const getDatabaseUrl = () => {
     return process.env.DATABASE_URL || 'mongodb://localhost:27017/express-ts-boilerplate'
 }
 
-export default {
+const envs = {
     port: parseInt(process.env.PORT || '3001'),
     host: process.env.HOST || 'localhost:3001',
     db: {
@@ -21,3 +21,31 @@ export default {
         jwt: process.env.JWT_SECRET || 'secret',
     },
 }
+
+const verifyEnv = () => {
+    if (!envs.db.DATABASE_URL) {
+        throw new Error('DATABASE_URL is not set')
+    }
+
+    if (!envs.secrets.jwt) {
+        throw new Error('JWT_SECRET is not set')
+    }
+
+    if (!envs.port) {
+        throw new Error('PORT is not set')
+    }
+
+    if (!envs.host) {
+        throw new Error('HOST URL is not set')
+    }
+
+    if (!envs.env) {
+        throw new Error('NODE_ENV is not set')
+    }
+
+    return true
+}
+
+verifyEnv()
+
+export default envs
